00e333231ae90a0b71e11404a03dc336284ce227
Kristoff Finley <contentconsuming@gmail.com>
2021-04-18 20:28:57 -0600

    Finishing Looking at Lamda Expressions

--- Hel/AdvancedC#/Lamda_Expressions.cs
+++ Hel/AdvancedC#/Lamda_Expressions.cs
@@ -14,6 +14,12 @@ namespace Hel.AdvancedCSharp
             le.ex1(); // 9
             le.ex2(); //100
 
+            //lamda expression instantiation
+            Func<int, bool> IsInputOne = x => x == 1;
+            le.ex4(IsInputOne, 1); //true
+            le.ex4(IsInputOne, 4); //false
+            le.ex5("Hello Everyone!"); //Hello Everyone!
+
         }
         //single parameter lamda expression
         public void ex1()
@@ -27,10 +33,22 @@ namespace Hel.AdvancedCSharp
             Double_Transformer multiply = (x, y) => x * y;
             System.Console.WriteLine(multiply(50,2)); //100
         }
-        //TODO look into Func<> and Action<> Delegates
+        //Func<input,return> Delegate Type
         public void ex3()
         {
-
+            Func<int,int> sqr = x => x * x;
+        }
+        //Delegate as input parameter
+        public void ex4(Func<int,bool> predicate, int input)
+        {
+            int Input = input;
+            System.Console.WriteLine(predicate(input));
+        }
+        //Action<T> Delegate Type
+        public void ex5(string my_word)
+        {
+            Action<string> PrintTheString = my_string => Console.WriteLine(my_string); 
+            PrintTheString(my_word);
         }
     }
 }
